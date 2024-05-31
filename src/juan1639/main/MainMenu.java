e563ae91315fce26167c4d766528e03f21ce7302
unknown <juaneguiabad1639@gmail.com>
2024-06-01 01:43:33 +0200

    Creado boton Play (a modo de MainMenu)

--- src/juan1639/main/MainMenu.java
+++ src/juan1639/main/MainMenu.java
@@ -5,7 +5,6 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.FontMetrics;
-import java.awt.GridLayout;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
@@ -39,72 +38,79 @@ public class MainMenu extends JFrame {
 	public void settingsJFrame() {
 
 		setSize(ANCHO_JFRAME, ALTO_JFRAME);
-		setTitle(" [ P U Z Z L E  9 ]  Menu Principal");
+		setTitle(" [ P U Z Z L E - 9 ]  Menu Principal");
 		setLocationRelativeTo(null);
-		setResizable(true);
-		setMinimumSize(new Dimension(300, 300));
-
+		setResizable(false);
+		setMinimumSize(new Dimension(300, 200));
 		// this.getContentPane().setBackground(Color.BLUE);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 	}
 
 	public void crearPanel() {
 
-		panel = new JPanel(new GridLayout(3, 1));
+		panel = new JPanel(new BorderLayout(0, 0));
 		//panel.setLayout(null);
-		panel.setBorder(BorderFactory.createLineBorder(Color.GRAY, 2, true));
+		
+		crearBotonJugar();
+		
+		// panel.setBorder(BorderFactory.createLineBorder(Color.GRAY, 2, true));
 		// panel.setBackground(Color.DARK_GRAY);
 		this.getContentPane().add(panel);
 	}
 
 	public void crearEtiquetas() {
-		
-		String txt = "P U Z Z L E  9";
+
+		String txt = "P U Z Z L E - 9";
 		int x = (int) (ANCHO_JFRAME / 2);
 
-		titulo = new JLabel(txt, SwingConstants.CENTER);
-		
-		Font fuente = new Font("arial", Font.BOLD, 50);
+		titulo = new JLabel(txt);
+
+		Font fuente = new Font("arial", Font.BOLD, 60);
 		titulo.setFont(fuente);
-		
-		//FontMetrics fm = titulo.getFontMetrics(fuente);
-		//int txtWidth = fm.stringWidth(txt);
+
+		FontMetrics fm = titulo.getFontMetrics(fuente);
+		int txtWidth = fm.stringWidth(txt);
 		
 		//titulo.setAlignmentX(SwingConstants.CENTER);
+		titulo.setHorizontalAlignment(SwingConstants.CENTER);
 		
 		/*
 		 * titulo.setBounds(x - (int) (txtWidth / 2), (int) (ALTO_JFRAME / 10), (int)
 		 * (ANCHO_JFRAME / 1.5), (int) (ALTO_JFRAME / 7));
 		 */
-		
+
 		titulo.setForeground(Settings.AZUL);
-		
+		// etiquetaGO.setOpaque(true);
+
 		panel.add(titulo);
-		//panel.add(titulo, BorderLayout.CENTER);
+		// panel.add(titulo, BorderLayout.CENTER);
 	}
 
 	public void crearBotonJugar() {
-
-		botonJugar = new JButton();
 		
-		botonJugar.setFont(new Font("arial", Font.BOLD, 30));
-		botonJugar.setText("Jugar");
+		botonJugar = new JButton("Play");
+		
+		botonJugar.setFont(new Font("arial", Font.BOLD, 45));
 		// botonNG.setSize((int) (goX / 1.3), (int) (goY / 8));
 		botonJugar.setFocusable(false);
 		botonJugar.setBorder(null);
+		//botonJugar.setMargin();
+		botonJugar.setBorder(BorderFactory.createEmptyBorder(
+				(int) (ALTO_JFRAME / 12), 0, (int) (ALTO_JFRAME / 12), 0));
+		
 		// botonJugar.requestFocus();
 		// botonNG.setBorder(BorderFactory.createLineBorder(Color.BLUE, 1, true));
-		
+
 		/*
 		 * botonJugar.setBounds((int) (ANCHO_JFRAME / 3), (int) (ALTO_JFRAME / 1.5),
 		 * (int) (ANCHO_JFRAME / 2.5), (int) (ALTO_JFRAME / 7));
 		 */
-		
+
 		// botonNG.setOpaque(true);
-		botonJugar.setForeground(Settings.AZUL);
-		botonJugar.setBackground(Settings.AZUL_CLARO);
-		// botonNG.setBackground(Color.RED);
-		panel.add(botonJugar);
+		botonJugar.setForeground(Settings.AZUL_CLARO);
+		
+		//panel.add(botonJugar);
+		panel.add(botonJugar, BorderLayout.SOUTH);
 	}
 
 	public MouseListener eventoBotonJugar() {
@@ -122,28 +128,21 @@ public class MainMenu extends JFrame {
 			}
 
 			@Override
-			public void mousePressed(MouseEvent e) {
-				// TODO Auto-generated method stub
-			}
+			public void mousePressed(MouseEvent e) {}
 
 			@Override
-			public void mouseReleased(MouseEvent e) {
-				// TODO Auto-generated method stub
-			}
+			public void mouseReleased(MouseEvent e) {}
 
 			@Override
 			public void mouseEntered(MouseEvent e) {
-				// TODO Auto-generated method stub
-				// botonJugar.setForeground(Color.GREEN);
-				botonJugar.setForeground(Settings.AMARILLO);
-				botonJugar.setBackground(Settings.AZUL);
+				
+				botonJugar.setForeground(Settings.AZUL);
 			}
 
 			@Override
 			public void mouseExited(MouseEvent e) {
-				// TODO Auto-generated method stub
-				botonJugar.setForeground(Settings.AZUL);
-				botonJugar.setBackground(Settings.AZUL_CLARO);
+				
+				botonJugar.setForeground(Settings.AZUL_CLARO);
 			}
 		};
 
--- src/juan1639/main/Settings.java
+++ src/juan1639/main/Settings.java
@@ -16,7 +16,7 @@ public class Settings {
 	public static final Color NEGRO = new Color(1, 1, 1);
 	public static final Color AMARILLO = new Color(245, 235, 1);
 	public static final Color BLANCO = new Color(247, 247, 247);
-	public static final Color AZUL = new Color(9, 125, 185);
-	public static final Color AZUL_CLARO = new Color(9, 225, 185);
+	public static final Color AZUL = new Color(9, 115, 190);
+	public static final Color AZUL_CLARO = new Color(9, 215, 190);
 	public static final Color VERDE = new Color(50, 195, 9);
 }
